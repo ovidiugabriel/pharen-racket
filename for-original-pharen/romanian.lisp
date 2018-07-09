1daf98f30681a22d69e9e34b5cbb7fce25d17d01
Ovidiu <ovidiugabriel@gmail.com>
2018-07-09 17:44:02 +0300

    Update romanian.lisp

--- for-original-pharen/romanian.lisp
+++ for-original-pharen/romanian.lisp
@@ -2,18 +2,14 @@
 ; romanian
 
 (defmacro daca (test-expr then-expr el-expr)
-    '(if ~test-expr ~then-expr ~el-expr)
-)
+    '(if ~test-expr ~then-expr ~el-expr) )
 
 (defmacro cand (test-expr body)
-    '(when ~test-expr ~body)
-)
+    '(when ~test-expr ~body) )
 
 (defmacro executa (&args)
-    '(do ~@args)    
-)
+    '(do ~@args) )
 
 ; unless
 (defmacro cand-nu (test-expr body)
-    '(when (not ~test-expr) ~body)
-)
+    '(when (not ~test-expr) ~body) )
